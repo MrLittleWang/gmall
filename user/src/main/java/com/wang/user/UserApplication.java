dcb309cc7f6946fd10b0d656c996d756b7544fe3
wangweijie <1216300049@qq.com>
2020-07-17 17:39:37 +0800

    user demo测试提交

--- user/src/main/java/com/wang/user/UserApplication.java
+++ user/src/main/java/com/wang/user/UserApplication.java
@@ -1,9 +1,11 @@
 package com.wang.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wang.user.dao")
 public class UserApplication {
 
     public static void main(String[] args) {
